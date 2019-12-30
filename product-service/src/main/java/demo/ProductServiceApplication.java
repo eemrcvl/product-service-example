1493f39ad8937bd933097c79abc4e44b59e510c7
Eyüp Emre Civil <45110817+eemrcvl@users.noreply.github.com>
2019-12-30 14:56:00 +0300

    Add files via upload

--- product-service/src/main/java/demo/ProductServiceApplication.java
+++ product-service/src/main/java/demo/ProductServiceApplication.java
@@ -2,9 +2,15 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProductServiceApplication {
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
     public static void main(String[] args){
         SpringApplication.run(ProductServiceApplication.class,args);
     }
